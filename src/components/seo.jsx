e28d1c8cb950c9280cf691ab133a47cb4a935844
zhouwei <weimizhoujoyce@gmail.com>
2020-12-02 00:49:47 +1030

    update

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -168,12 +168,7 @@ const SEO = ({
             <meta property="og:title" content={seo.title} />
             <meta property="og:description" content={seo.description} />
             <meta property="og:image" content={seo.image} />
-            {/* Twitter Tags */}
-            <meta name="twitter:card" content="summary" />
            
-            <meta name="twitter:title" content={seo.title} />
-            <meta name="twitter:description" content={seo.description} />
-            <meta name="twitter:image" content={seo.image} />
             {/* Schema Org */}
             {!isArticle && (
               <script type="application/ld+json">
