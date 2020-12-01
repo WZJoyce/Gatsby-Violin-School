f71176475ad9308261ebca44bf3ce882d918f990
zhouwei <weimizhoujoyce@gmail.com>
2020-12-01 21:32:32 +1030

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,10 @@ module.exports = {
     bannerImage: "/images/banner.png",
     blogPostsPerPage: 5,
     social: {
-      instagram: "",
-      twitter: "",
+      instagram: "https://github.com/WZJoyce",
+      twitter: "https://github.com/WZJoyce",
     },
-    mailchimpUrl: "",
+    mailchimpUrl: "https://github.com/WZJoyce",
   },
   plugins: [
     {
